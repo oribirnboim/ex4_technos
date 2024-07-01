83c03d8bf52818d5fd1a4c744f9061acd942cee3
oribirnboim <ori.birnboim.44@gmail.com>
2024-07-01 15:20:45 +0300

    finished writing bonus, not tested yet!!

---
+++ spoof_detector.py
@@ -0,0 +1,49 @@
+from scapy.layers.l2 import ARP
+from scapy.layers.dns import IP
+import scapy
+import scapy.packet
+import warnings
+
+STATIC_ARP_DICT = {
+    "10.0.2.43": "12:34:56:12:34:56",
+    "8.8.8.8": "52:54:00:12:35:00"
+}
+
+
+class SpoofDetector(object):
+    def __init__(self, static_arp_dict) -> None:
+        self.static_arp_dict = static_arp_dict
+    
+    def suspicious(self, pkt: scapy.packet.Packet) -> bool:
+        ip_list = list(self.static_arp_dict.keys())
+        if ARP in pkt and pkt[ARP].op == 2: #if it is an arp response packet
+            ip = pkt[IP].src
+            mac = pkt[ARP].hwsrc
+            return ip in ip_list and mac != self.static_arp_dict[ip]
+    
+    def attacker_ip(self, pkt: scapy.packet.Packet) -> str:
+        return pkt[IP].src
+    
+    def resolve_packet(self, pkt: scapy.packet.Packet) -> None:
+        if self.suspicious(pkt):
+            attacker_ip = self.attacker_ip(pkt)
+            warnings.warn(f"detecetd arp poisening from ip address {attacker_ip}")
+        return
+
+
+    def run(self) -> None:
+        while True:
+            try:
+                scapy.sniff(filter = "arp", prn=self.resolve_packet, store=0)
+            except:
+                import traceback
+                traceback.print_exc()
+
+    def start(self) -> None:
+        while True:
+            self.run()
+
+
+if __name__ == "__main__":
+    spoof_detector = SpoofDetector(STATIC_ARP_DICT)
+    spoof_detector.start()
